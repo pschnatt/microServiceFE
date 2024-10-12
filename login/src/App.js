4055722a7cb2696d4d2585e77a9c3ef7c7eb8247
HariTTira <haritpao@gmail.com>
2024-10-12 20:27:38 +0700

    fix login

--- login/src/App.js
+++ login/src/App.js
@@ -1,6 +1,92 @@
-import React, { Suspense, useState } from "react";
-import "./App.css";
-// const CartApp = React.lazy(() => import('app2/App'));
+// import React, { Suspense, useState } from "react";
+// import "./LoginPage.css";
+// // const CartApp = React.lazy(() => import('app2/App'));
+
+// const App = () => {
+//     const [data, setData] = useState({
+//         email: "",
+//         password: "",
+//     });
+
+//     // const navigate = useNavigate();
+
+//     const handleLogin = async (e) => {
+//         // e.preventDefault();
+//         // const { email, password } = data;
+//         // try {
+//         //     const { data } = await axios.post(
+//         //         "/login",
+//         //         {
+//         //             email,
+//         //             password,
+//         //         },
+//         //         {
+//         //             withCredentials: true,
+//         //         }
+//         //     );
+//         //     if (data.error) {
+//         //         toast.error(data.error);
+//         //     } else {
+//         //         setData({});
+//         //         toast.success("Login Successful!");
+//         //         navigate('/');
+//         //     }
+//         // } catch (error) {
+//         //     console.log(error);
+//         // }
+//         console.log("Successfully Click the Login button");
+//     };
+//     return (
+//         <div className="main">
+//             <h1>Sign</h1>
+//             <h3>Enter your login credentials</h3>
+//             <form onSubmit={handleLogin}>
+//                 <label htmlFor="first">
+//                     Username:
+//                 </label>
+//                 <input
+//                     type="text"
+//                     id="first"
+//                     name="first"
+//                     placeholder="Enter your Username"
+//                     value={data.email}
+//                     onChange={(e) => setData({ ...data, email: e.target.value })}
+//                     required
+//                 />
+
+//                 <label htmlFor="password">
+//                     Password:
+//                 </label>
+//                 <input
+//                     type="password"
+//                     id="password"
+//                     name="password"
+//                     placeholder="Enter your Password"
+//                     value={data.password}
+//                     onChange={(e) => setData({ ...data, password: e.target.value })}
+//                     required
+//                 />
+
+//                 <div className="wrap">
+//                     <button type="submit">
+//                         Submit
+//                     </button>
+//                 </div>
+//             </form>
+
+//             <p>Not registered? 
+//                 <a href="#" style={{ textDecoration: "none" }}>
+//                     Create an account
+//                 </a>
+//             </p>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+import React, { useState } from "react";
+import "./LoginPage.css";
 
 const App = () => {
     const [data, setData] = useState({
@@ -8,82 +94,57 @@ const App = () => {
         password: "",
     });
 
-    // const navigate = useNavigate();
-
     const handleLogin = async (e) => {
-        // e.preventDefault();
-        // const { email, password } = data;
-        // try {
-        //     const { data } = await axios.post(
-        //         "/login",
-        //         {
-        //             email,
-        //             password,
-        //         },
-        //         {
-        //             withCredentials: true,
-        //         }
-        //     );
-        //     if (data.error) {
-        //         toast.error(data.error);
-        //     } else {
-        //         setData({});
-        //         toast.success("Login Successful!");
-        //         navigate('/');
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        // }
-        console.log("Successfully Click the Login button");
+        e.preventDefault();
+        console.log("Successfully clicked the Login button");
     };
-    return (
-        <div className="container">
-            <div className="forms-container">
-                <div className="form-control signin-form">
-                    <form>
-                        <h2>Signin</h2>
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            value={data.username}
-                            onChange={(e) =>
-                                setData({ ...data, username: e.target.value })
-                            }
-                            required
-                        />
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={data.password}
-                            onChange={(e) =>
-                                setData({ ...data, password: e.target.value })
-                            }
-                            required
-                        />
-                        <button type="button" onClick={handleLogin}>
-                            Signin
-                        </button>
-                    </form>
-                    <span>or signin with</span>
-                    <div className="socials">
-                        <i className="fab fa-facebook-f"></i>
-                        <i className="fab fa-google-plus-g"></i>
-                        <i className="fab fa-linkedin-in"></i>
-                    </div>
-                </div>
-            </div>
 
-            <div className="intros-container">
-                <div className="intro-control signin-intro">
-                    <div className="intro-control__inner">
-                        <h2>Welcome back!</h2>
-                        <p>
-                            Welcome back! We are so happy to have you here. It's great to see you again. We hope you had a safe and enjoyable time away.
-                        </p>
-                        <button id="signup-btn">No account yet? Signup.</button>
-                    </div>
+    return (
+        <div className="loginPage-container">
+            <h1 className="loginPage-title">Sign In</h1>
+            <h3>Enter your login credentials</h3>
+            <form onSubmit={handleLogin}>
+                <label htmlFor="first" className="loginPage-label">
+                    Username:
+                </label>
+                <input
+                    type="text"
+                    id="first"
+                    name="first"
+                    placeholder="Enter your Username"
+                    className="loginPage-input"
+                    value={data.email}
+                    onChange={(e) => setData({ ...data, email: e.target.value })}
+                    required
+                />
+
+                <label htmlFor="password" className="loginPage-label">
+                    Password:
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your Password"
+                    className="loginPage-input"
+                    value={data.password}
+                    onChange={(e) => setData({ ...data, password: e.target.value })}
+                    required
+                />
+
+                <div className="loginPage-wrap">
+                    <button type="submit" className="loginPage-button">
+                        Submit
+                    </button>
                 </div>
-            </div>
+            </form>
+
+            <p>
+                Not registered? 
+                <a href="#" style={{ textDecoration: "none" }}>
+                    Create an account
+                </a>
+            </p>
         </div>
     );
 };
--- shell/src/App.js
+++ shell/src/App.js
@@ -15,6 +15,17 @@ const Bookingde = lazy(() => import('mfe6/App'));
 const Restaurantdetail = lazy(() => import('mfe7/App'));
 const Confirmbooking = lazy(() => import('mfe8/App'));
 
+const NavbarWrapper = () => {
+    const location = useLocation();
+    
+    // Routes where the Navbar should NOT be displayed
+    const noNavbarRoutes = [ '/register', '/login'];
+
+    const shouldShowNavbar = !noNavbarRoutes.includes(location.pathname);
+
+    return shouldShowNavbar ? <Navbar /> : null;
+};
+
 // Component to conditionally render the Header
 const HeaderWrapper = () => {
     const location = useLocation();
@@ -30,20 +41,18 @@ const App = () => {
     return (
         <Router>
             <div>
-                <Navbar />
+                <NavbarWrapper />
                 <HeaderWrapper />
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/mfe1">Micro Frontend 1</Link></li>
-                        <li><Link to="/mfe2">Micro Frontend 2</Link></li>
+                        <li><Link to="/login">Micro Frontend 1</Link></li>
+                        <li><Link to="/register">Micro Frontend 2</Link></li>
                         <li><Link to="/bookinghistory">Booking History</Link></li>
                         <li><Link to="/bookingdetail">Booking Detail</Link></li> {/* New link */}
                         <li><Link to="/completeBooking">Complete Booking</Link></li> {/* New link */}
                         <li><Link to="/restaurantde">Restaurant Detail</Link></li> {/* New link */}
                         <li><Link to="/restaurant">Restaurant List</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li>
                     </ul>
                 </nav>
 
@@ -51,15 +60,14 @@ const App = () => {
                 <Suspense fallback={<div>Loading...</div>}>
                     <Routes>
                         <Route path="/" element={<Home />}/>
-                        <Route path="/mfe1" element={<Mfe1 />} />
-                        <Route path="/mfe2" element={<Mfe2 key="mfe2" />} />
+                        <Route path="/login" element={<Mfe1 />} />
+                        <Route path="/register" element={<Mfe2 key="mfe2" />} />
                         <Route path="/bookinghistory" element={<Mfe3 key="mfe3" />} />
                         <Route path="/bookingdetail" element={<Bookingde />} /> {/* Route for booking detail */}
                         <Route path="/completeBooking" element={<Confirmbooking />} /> {/* Route for complete booking */}
                         <Route path="/restaurantde" element={<Restaurantdetail />} /> {/* Route for restaurant detail */}
                         <Route path="/restaurant" element={<Mfe4 />} />
                         <Route path="/login" element={<LoginForm />} /> 
-                        <Route path="/register" element={<SignUpForm />} />
                     </Routes>
                 </Suspense>
             </div>
